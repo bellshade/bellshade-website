3cddbc42b1dd5e5a2e7d4e78a754de1cbd008d63
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-09-22 16:21:02 +0700

    fix: mengubah sumber api ke server bellshade

--- src/js/githubApiWrapper.js
+++ src/js/githubApiWrapper.js
@@ -2,8 +2,4 @@ import constant from './constant'
 
 const getContents = (url) => fetch(url).then((res) => res.json())
 
-export const getPublicMembers = () =>
-  getContents('https://api.github.com/orgs/bellshade/public_members')
-
-export const getUserInfo = (username) =>
-  getContents(`https://api.github.com/users/${username}`)
+export const getPublicMembers = () => getContents(constant.api)
