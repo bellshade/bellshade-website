e86a27ea5cfa7e937e1bd9d99d8e9a061ef59ae5
Ezra Khairan Permana <48118327+reacto11mecha@users.noreply.github.com>
2021-10-23 18:39:07 +0700

    Migrate api (#60)
    * fix: memperbaiki duplikat dependencies dan package.json yang terdampak eslint
    
    * fix: memindahkan api dari domain heroku ke domain org

--- src/js/constant.js
+++ src/js/constant.js
@@ -1,7 +1,7 @@
 const constant = {
   dummyImage:
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAAAXNSR0IArs4c6QAAAPtJREFUeF7t0rENAAAMwrDy/9M9wmvYs1jsGgmM6uILEE8QYIAogHkPDBAFMO+BAaIA5j0wQBTAvAcGiAKY98AAUQDzHhggCmDeAwNEAcx7YIAogHkPDBAFMO+BAaIA5j0wQBTAvAcGiAKY98AAUQDzHhggCmDeAwNEAcx7YIAogHkPDBAFMO+BAaIA5j0wQBTAvAcGiAKY98AAUQDzHhggCmDeAwNEAcx7YIAogHkPDBAFMO+BAaIA5j0wQBTAvAcGiAKY98AAUQDzHhggCmDeAwNEAcx7YIAogHkPDBAFMO+BAaIA5j0wQBTAvAcGiAKY98AAUQDzHoiAD0PoAFE/HRk+AAAAAElFTkSuQmCC', // Dummy image 80px png transparent
-  api: 'https://bellshade-server.herokuapp.com/',
+  api: 'https://api.bellshade.org/',
 }
 
 export default constant
